d4cd4838b1d8d8d74fbce62d95e19f4973bcda65
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2025-07-04 14:45:50 +0200

    Cool Shaders

--- Scripts/GlobalFunctions.cs
+++ Scripts/GlobalFunctions.cs
@@ -14,5 +14,13 @@ namespace BulletHellGD.Scripts
             Random Rand = new Random();
             return new Color(Rand.Next(0, 100) / 100f, Rand.Next(0, 100) / 100f, Rand.Next(0, 100) / 100f);
         }
+
+        public static float GetbrightestColor(Color _Color)
+        {
+            if (_Color.R > _Color.G && _Color.R > _Color.B) return _Color.R;
+            if (_Color.G > _Color.R && _Color.G > _Color.B) return _Color.G;
+            if (_Color.B > _Color.R && _Color.B > _Color.G) return _Color.B;
+            return 1f;
+        }
     }
 }
--- Scripts/Platforms/NormalPlatform.cs
+++ Scripts/Platforms/NormalPlatform.cs
@@ -8,7 +8,17 @@ public partial class NormalPlatform : Node2D
 	{
 		base._EnterTree();
 
-		MeshInstance2D Mesh = GetNode<MeshInstance2D>("StaticBody2D/MeshInstance2D");
-		Mesh.Modulate = GlobalFunctions.RandomColor();
-	}
+		GetNode<Sprite2D>("StaticBody2D/Sprite").Modulate = GlobalFunctions.RandomColor();
+
+        ShaderMaterial mat = GetNode<Sprite2D>("StaticBody2D/Sprite").Material as ShaderMaterial;
+        if (mat != null)
+        {
+            if (GlobalFunctions.GetbrightestColor(GetNode<Sprite2D>("StaticBody2D/Sprite").Modulate) > 0.6f)
+            {
+                float glowstr = (float)mat.GetShaderParameter("glow_strength");
+                glowstr *= 0.8f;
+                mat.SetShaderParameter("glow_strength", glowstr);
+            }
+        }
+    }
 }
